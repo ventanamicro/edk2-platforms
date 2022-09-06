58dc543800f32c15d38b9a930d036a1d2b10cc14
Tuan Phan <tphan@ventanamicro.com>
2022-09-06 11:26:35 -0700

    Platform/Orbiter: Initial support for Cadence I2C
    Add Cadence I2C driver that be used in Orbiter platform.
    
    Signed-off-by: Tuan Phan <tphan@ventanamicro.com>

---
+++ Platform/VentanaMicro/Orbiter/Include/Platform/MemoryMap.h
@@ -0,0 +1,16 @@
+/** @file
+  Memory map definition of Orbiter platform.
+
+  Copyright (c) 2017, Linaro, Ltd. All rights reserved.<BR>
+  Copyright (c) 2022, Ventana Micro Systems Inc. All Rights Reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef MEMORY_MAP_H
+#define MEMORY_MAP_H
+
+#define ORBITER_SYS_I2C_BASE  0x00000000 /* FIXME: */
+#define ORBITER_SYS_I2C_SIZE  0x1000 /* FIXME: */
+
+#endif /* MEMORY_MAP_H_ */
